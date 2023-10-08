67cceeae6692ba3ea7fc9348d95cd1662bb23d44
Ayush Sharma <ayush73618@gmail.com>
2023-10-08 13:06:48 +0530

    css commit

--- src/components/HomePage/ShowItem.js
+++ src/components/HomePage/ShowItem.js
@@ -1,31 +1,48 @@
-import React from 'react'
-import classes from './ShowItem.module.css';
-import ShowItems from './ShowItems';
+import React from "react";
+import classes from "./ShowItem.module.css";
+import ShowItems from "./ShowItems";
 const ShowItem = (props) => {
   return (
-    
-    <div className={`${classes['show-menu']}`  }>
-     <div>
-        <h2 style={{'marginLeft':'1rem',
-      'textAlign':'center'}}>
-          {props.type}
-        </h2>
-        
-     </div>
-       <ShowItems img={props.pictureArray[0]} tagline="Starting from 6999" types="Mobile" />
-       <ShowItems img={props.pictureArray[1]} tagline="Starting from 9999" types="Camera"  />
-       <ShowItems img={props.pictureArray[2]} tagline="Starting from 499" types="Earphones" />
-       <ShowItems img={props.pictureArray[3]} tagline="Starting from 34999" types='Laptop'  />
-       <ShowItems img={props.pictureArray[4]} tagline="Starting from 2999" types="Watches"  />
-
-
+    <div className={classes["show-menu-boxes"]}>
+      <h4 style={{ textAlign: "center", marginTop: "0.5rem" }}>{props.type}</h4>
+      <div className={`${classes["show-menu"]}`}>
+        {/* <h2 style={{ marginLeft: "1rem", textAlign: "center" }}>{props.type}</h2> */}
+        {/* <ShowItems
+          img={props.pictureArray[0]}
+          tagline="Starting from 6999"
+          types="Mobile"
+        />
+        <ShowItems
+          img={props.pictureArray[1]}
+          tagline="Starting from 9999"
+          types="Camera"
+        /> */}
+        <ShowItems
+          img={props.pictureArray[2]}
+          tagline="Starting from 499"
+          types="Earphones"
+        />
+        <ShowItems
+          img={props.pictureArray[3]}
+          tagline="Starting from 34999"
+          types="Laptop"
+        />
+        <ShowItems
+          img={props.pictureArray[4]}
+          tagline="Starting from 2999"
+          types="Watches"
+        />
+        <ShowItems
+          img={props.pictureArray[4]}
+          tagline="Starting from 2999"
+          types="Watches"
+        />
+      </div>
     </div>
-  
-  )
-}
+  );
+};
 
-export default ShowItem
-
+export default ShowItem;
 
 // style={{'display':'flex',
 //      'height':'100%',
@@ -38,5 +55,5 @@ export default ShowItem
 //      'color':'#fff',
 //      'borderRadius':'12px',
 //      'marginLeft':'0',
-     
+
 //      }}
