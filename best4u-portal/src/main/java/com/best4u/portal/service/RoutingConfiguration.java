6643cea9ef4a36f898814c915c7668b87e304965
Ayush Sharma <ayush73618@gmail.com>
2024-09-30 23:38:50 +0530

    Commit some changes for routing to Configuration-services

---
+++ best4u-portal/src/main/java/com/best4u/portal/service/RoutingConfiguration.java
@@ -0,0 +1,9 @@
+package com.best4u.portal.service;
+
+import org.springframework.http.ResponseEntity;
+
+public interface RoutingConfiguration {
+	
+	public ResponseEntity<?> getUserDetail();
+
+}
---
+++ best4u-portal/src/main/java/com/best4u/portal/service/impl/RoutingConfigurationImpl.java
@@ -0,0 +1,17 @@
+package com.best4u.portal.service.impl;
+
+import org.springframework.http.ResponseEntity;
+
+import com.best4u.portal.service.RoutingConfiguration;
+
+public class RoutingConfigurationImpl implements RoutingConfiguration {
+
+	@Override
+	public ResponseEntity<?> getUserDetail() {
+		// TODO Auto-generated method stub
+		 
+		return null;
+	}
+
+	
+}
